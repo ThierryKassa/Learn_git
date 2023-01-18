77415e45d6fe94d385428b579280d312f2429bda
Thierry Kas <bigkatiess2@gmail.com>
2023-01-18 16:09:52 +0800

    WELCOME AT THE REPOSITORY

---
+++ src/Sudoko.java
@@ -0,0 +1,11 @@
+public class Sudoko{
+	static int[][] game;
+
+    public static void main(String[] args) {
+        int [][] games= new int[3][3];
+        int [] play = new int[3];
+        Object obj =games;
+        obj = play;
+        System.out.println(obj);
+    }
+}
